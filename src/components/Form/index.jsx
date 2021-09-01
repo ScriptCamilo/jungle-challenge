7ed0aa9e31e2354a4796a6814ea430b54441c659
ScriptCamilo <script.camilo@gmail.com>
2021-09-01 14:58:42 -0300

    feat: form component created

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function Form() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  return (
+    <form>
+      <input type="" />
+      <input type="" />
+      <button type="submit" />
+    </form>
+  );
+}
+
+export default Form;
