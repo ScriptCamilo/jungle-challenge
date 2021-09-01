33073c480a0d1a0d5c54829deeddf105839ad6b7
ScriptCamilo <script.camilo@gmail.com>
2021-09-01 07:40:03 -0300

    feat: nanny component finished

--- src/components/Nanny/index.jsx
+++ src/components/Nanny/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 import nanny from '../../assets/images/nanny.svg';
+import styles from './styles.module.css';
 
 function Nanny() {
   return (
-    <section className="container">
-      <a href="#nanny">
+    <section className={ `container ${styles.nannyContainer}`} >
+      <a href="#nanny" className={ styles.link }>
         <img src={ nanny } alt="Nanny example" />
+        <p>
+          <span>
+            Sarah's day care available
+            now in North Sydney
+          </span>
+        </p>
       </a>
+
+      <h5>Wednesday, Thursday, Friday - 7:30 - 5:30</h5>
     </section>
   );
 }
