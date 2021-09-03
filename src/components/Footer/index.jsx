1f51c3ddfffce835b8096abdd859bfad8c4959e9
ScriptCamilo <script.camilo@gmail.com>
2021-09-02 21:58:58 -0300

    feat: social medias icons added

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,6 +5,9 @@ import CustomLink from '../Link';
 
 import calendarIcon from '../../assets/icons/calendar.svg';
 import hapuLogo from '../../assets/icons/logo.svg';
+import facebook from '../../assets/icons/facebook.svg';
+import twitter from '../../assets/icons/twitter.svg';
+import instagram from '../../assets/icons/instagram.svg';
 import styles from './styles.module.css';
 
 function Footer() {
@@ -29,23 +32,29 @@ function Footer() {
       </Button>
 
       <CustomLink 
-        href="#browse-nanny"
+        href="browse-nanny"
         link="Or browse local nanny-shares"
       />
 
       <img src={ hapuLogo } alt="Logo Hapu" />
 
-      <section>
-        <a href="#share-your-nanny">Share Your Nanny</a>
-        <a href="#our-story">Our Story</a>
-        <a href="#blog">Blog</a>
-        <a href="#terms-privacy">Terms &amp; Privacy</a>
+      <section className={ styles.links }>
+        <a href="share-your-nanny"><h5>Share Your Nanny</h5></a>
+        <a href="our-story"><h5>Our Story</h5></a>
+        <a href="blog"><h5>Blog</h5></a>
+        <a href="terms-privacy"><h5>Terms &amp; Privacy</h5></a>
       </section>
 
-      <section>
-        <img src="" alt="Facebook icon" />
-        <img src="" alt="Twitter icon" />
-        <img src="" alt="Instagram icon" />
+      <section className={ styles.socialMedia }>
+        <a href="https://facebook.com" target="_blank" rel="noreferrer">
+          <img src={ facebook } alt="Facebook icon" />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noreferrer">
+          <img src={ twitter } alt="Twitter icon" />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noreferrer">
+          <img src={ instagram } alt="Instagram icon" />
+        </a>
       </section>
 
       <hr />
