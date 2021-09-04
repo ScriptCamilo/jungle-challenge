eadcb24e3094a4b2e2ef4fdf2355b3db6db55450
ScriptCamilo <script.camilo@gmail.com>
2021-09-04 07:59:52 -0300

    feat: Section component now accepts others classes

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import styles from './styles.module.css';
 
-function Section({ children }) {
+function Section({ children, className }) {
   return (
-    <section className={ styles.mainSection }>
+    <section className={ `${styles.mainSection} ${className}` }>
       { children }
     </section>
   );
