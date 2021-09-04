15548d58af497694f493d7c7e9c41f70302108ea
ScriptCamilo <script.camilo@gmail.com>
2021-09-04 07:58:35 -0300

    feat: adding media query for 768px

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,6 +9,7 @@ import Footer from '../../components/Footer';
 
 import imageSection1 from '../../assets/images/imageSection1.svg';
 import imageSection3 from '../../assets/images/imageSection3.svg';
+import imageSection4 from '../../assets/images/imageSection4.svg';
 import imageSection5 from '../../assets/images/imageSection5.svg';
 
 import styles from './styles.module.css';
@@ -21,35 +22,37 @@ function Home() {
       <Profile />
 
       <main>
-        <Section>
+        <Section className={  styles.rowSection } >
           <img src={ imageSection1 } alt="Interface example using a notebook" />
 
-          <h3 className={ styles.shareYourHomeTitle}>
-            Share your home,
-            nanny and costs
-          </h3>
+          <div>
+            <h3 className={ styles.shareYourHomeTitle}>
+              Share your home,
+              nanny and costs
+            </h3>
 
-          <p>
-            You have a fantastic home, a fantastic 
-            nanny and wouldn’t cutting your costs in 
-            half be, well, fantastic?! If only it was easy 
-            to connect with other parents to share 
-            your costs? Well now it is, with Hapu.&nbsp;
-            <CustomLink href="#tribe" link="Hapu means tribe" />
-            &nbsp;and it’s our foundational 3 
-            tribal principles that empowers you to 
-            create and manage your own tribe. A tribe 
-            that together has the power to create new 
-            affordable solutions in childcare that work 
-            for you and your community.
-          </p> <br />
+            <p>
+              You have a fantastic home, a fantastic 
+              nanny and wouldn’t cutting your costs in 
+              half be, well, fantastic?! If only it was easy 
+              to connect with other parents to share 
+              your costs? Well now it is, with Hapu.&nbsp;
+              <CustomLink href="#tribe" link="Hapu means tribe" />
+              &nbsp;and it’s our foundational 3 
+              tribal principles that empowers you to 
+              create and manage your own tribe. A tribe 
+              that together has the power to create new 
+              affordable solutions in childcare that work 
+              for you and your community.
+            </p> <br />
 
-          <CustomLink href="#ready" link="Ready to get started?" />
+            <CustomLink href="#ready" link="Ready to get started?" />
+          </div>
         </Section>
 
         <hr />
 
-        <Section>
+        <Section className={ styles.form }>
           <h3 className={ styles.formTitle }>
             Are you a parent without a 
             nanny and looking to share?
@@ -66,28 +69,30 @@ function Home() {
 
         <hr />
 
-        <Section>
+        <Section className={ styles.rowSection }>
           <img src={ imageSection3 } alt="How to choose your price amount" />
 
-          <h3 className={ styles.paymentShareTitle }>
-            Shared payments
-            made simple
-          </h3>
+          <div>
+            <h3 className={ styles.paymentShareTitle }>
+              Shared payments
+              made simple
+            </h3>
 
-          <p>
-            Sometimes it’s hard enough just sharing a 
-            restaurant bill. Try sharing that bill week in,
-            week out and you might encounter more 
-            than a few snares. But not with Hapu. 
-            Simply set your rates and our automated 
-            payment system takes care of the rest. You 
-            need never talk money or who owes what.
-          </p> <br />
+            <p>
+              Sometimes it’s hard enough just sharing a 
+              restaurant bill. Try sharing that bill week in,
+              week out and you might encounter more 
+              than a few snares. But not with Hapu. 
+              Simply set your rates and our automated 
+              payment system takes care of the rest. You 
+              need never talk money or who owes what.
+            </p> <br />
 
-          <CustomLink 
-            href="#payment-share"
-            link="Read how Bridget's share (without Hapu) ended over $15"
-          />
+            <CustomLink 
+              href="#payment-share"
+              link="Read how Bridget's share (without Hapu) ended over $15"
+            />
+          </div>
         </Section>
 
         <hr />
@@ -111,6 +116,12 @@ function Home() {
           <CustomLink 
             href="#framework"
             link="Read how Hapu's tribal background defines our app"
+          />
+
+          <img 
+            src={ imageSection4 }
+            alt="Sample of billing history"
+            className={ styles.billingImage }
           />
         </Section>
 
