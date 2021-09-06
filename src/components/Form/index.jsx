5967624c6b0aa295979addf1c6818d2c454b7e5f
ScriptCamilo <script.camilo@gmail.com>
2021-09-06 15:16:10 -0300

    feat: form finished

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,53 +1,149 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { RiCheckLine, RiErrorWarningLine } from 'react-icons/ri';
 
-import Button from '../Button';
-
-import styles from './styles.module.css';
+import { postForm } from '../../services/jungleApi';
 import { isEmailValid } from '../../utils/validations';
 
+import Button from '../Button';
+import Loading from '../Loading';
+
+import styles from './styles.module.css';
+
 function Form() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const [inputName, setInputName] = useState('');
+  const [isNameError, setIsNameError] = useState(false);
 
-  const handleChange = ({target: { value }}, setValue) => {
-    setValue(value)
-  }
+  const [inputEmail, setInputEmail] = useState('');
+  const [isEmailError, setIsEmailError] = useState(false);
+  const [emailErrorMsg, setEmailErrorMsg] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const [formStatus, setFormStatus] = useState({
+    sent: false,
+    ok: false,
+  });
 
-    if (!isEmailValid(email)) {
-      return console.log('error');
+
+  const handleChange = ({target: { value, name }}) => {
+    if (name === 'name') {
+      setIsNameError(false);
+
+      if (inputName === '' && value === ' ') {
+        return setInputName('');
+      }
+      return setInputName(value);
     }
 
-    return console.log('success');
+    // PREVENT THE USER TO TYPE SPACES
+    const emailValue = value.split(' ').join('');
+
+    setInputEmail(emailValue);
+    setIsEmailError(false);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (inputName === '') return setIsNameError(true);
+    if (inputEmail === '') {
+      setEmailErrorMsg('Your email is required');
+      return setIsEmailError(true);
+    }
+
+    
+    if (!isEmailValid(inputEmail)) {
+      setInputEmail('');
+      setEmailErrorMsg('Your email must be valid');
+      return setIsEmailError(true);
+    }
+
+    setIsLoading(true);
+
+    try {
+      await postForm({ name: inputName, email: inputEmail });
+      setIsLoading(false);
+      setFormStatus({ sent: true, ok: true });
+
+    } catch (error) {
+      setIsLoading(false);
+      setFormStatus({ sent: true, ok: false })
+      console.error(error);
+    }
+
+    setInputName('');
+    setInputEmail('');
+  }
+
+  const handleFeedback = () => {
+    setTimeout(() => {
+      setFormStatus({sent: false, ok: false})
+    }, 5000)
+
+    if (formStatus.ok) {
+      return (
+        <p className={ styles.ok }>
+          <RiCheckLine size="1.5rem" /> &nbsp;
+          <span>We received with success!</span>
+        </p>
+      );
+    }
+
+    return (
+      <p className={ styles.error }> 
+        <RiErrorWarningLine size="1.5rem" /> &nbsp;
+        <span>Something went wrong, please try again! </span>
+      </p>
+    )
   }
 
   return (
-    <form className={ styles.form }>
+    <form className={ styles.form } onSubmit={ handleSubmit }>
       <label htmlFor="name" className={ styles.label }>
         <input 
           type="text"
           id="name"
-          value={ name }
-          onChange={ (event) => handleChange(event, setName) }
-          placeholder="Your name" 
+          value={ inputName }
+          name="name"
+          onChange={ handleChange }
+          placeholder="Your name *"
+          className={ isNameError ? styles.requiredError : '' }
         />
+        {
+          !isNameError ? '' : (
+            <span className={ styles.required }>
+              Your name is required
+            </span>
+          )
+        }
       </label>
 
       <label htmlFor="email" className={ styles.label }>
         <input 
           type="text"
           id="email"
-          value={ email }
-          onChange={ (event) => handleChange(event, setEmail) }
-          placeholder="Your email"
+          value={ inputEmail }
+          name="email"
+          onChange={ handleChange }
+          placeholder="Your email *"
+          className={ isEmailError ? styles.requiredError : '' }
         />
+        {
+          !isEmailError ? '' : (
+            <span className={ styles.required }>
+              {emailErrorMsg}
+            </span>
+          )
+        }
       </label>
 
-      <Button className={ styles.button } onClick={ handleSubmit }>
-        Send
+      <Button isSubmit className={ styles.button }>
+        { isLoading ? <Loading /> : 'Send' }
       </Button>
+
+      {
+        !formStatus.sent ? '' : handleFeedback()
+      }
     </form>
   );
 }
--- src/services/jungleApi.js
+++ src/services/jungleApi.js
@@ -1,7 +1,14 @@
 const API = 'https://api.jungledevs.com/api/v1/challenge-newsletter/';
 
-function postHomeForm() {
-
+export async function postForm(credentials) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentials),
+  }
+  const response = await fetch(API, options);
+  
+  if (!response.ok) throw new Error('Something went wrong');
 }
-
-export default postHomeForm;
